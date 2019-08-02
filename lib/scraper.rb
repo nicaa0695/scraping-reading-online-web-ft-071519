78fd697247e2306a493a16364d9305fefdc06442
Veronica Lopez <nicaa0695@gmail.com>
2019-08-02 19:51:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+puts doc.css(".site-header__hero__headline").text.strip
 
-html = open("https://flatironschool.com/")
 
 
 
